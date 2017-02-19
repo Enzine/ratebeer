a22142ff1210b5468bb57ea9b35c5434c26d2dda
Enzine <sonja.makipaja@kolumbus.fi>
2017-02-20 00:36:57 +0200

    fixed tests

--- lib/beerweather_api.rb
+++ lib/beerweather_api.rb
@@ -4,13 +4,14 @@ class BeerweatherApi
     url = "http://api.apixu.com/v1/current.json?key=#{key}&q=#{city}"
 
     response = HTTParty.get "#{url}"
+    return nil if response.parsed_response.has_key? "error"
     weather = {}
     weather[:temperature] = response.parsed_response["current"]["temp_c"]
     weather[:feelslike] = response.parsed_response["current"]["feelslike_c"]
     weather[:text] = response.parsed_response["current"]["condition"]["text"]
     weather[:icon] = response.parsed_response["current"]["condition"]["icon"]
     weather[:wind] = response.parsed_response["current"]["wind_mph"]
-    weather    
+    weather
   end
 
   def self.key
--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 describe "Places" do
+  before :each do
+    allow(BeerweatherApi).to receive(:fetch_weather_in).with("kumpula").and_return(
+       {
+         temperature: 50,
+         feelslike: 100,
+         text: "boiling",
+         icon: "www.google.fi",
+         wind: 1000000
+       }
+    )
+  end
   it "if one is returned by the API, it is shown at the page" do
     allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
       [ Place.new( name:"Oljenkorsi", id: 1 ) ]
     )
-
     visit places_path
     fill_in('city', with: 'kumpula')
     click_button "Search"
